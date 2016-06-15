67ffb7bd610fd47ef84eb939d6e5bff8894cb724
zaifoski <sofiachimirri@gmail.com>
2016-06-15 16:15:59 +0200

    remove fragment from category activity

--- app/src/main/java/com/example/eatmeet/activities/CategoryActivity.java
+++ app/src/main/java/com/example/eatmeet/activities/CategoryActivity.java
@@ -35,53 +35,29 @@ public class CategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
-
-    public static class PlaceholderFragment extends Fragment {
 
         ArrayAdapter<String> categoryAdapter;
+        CategoryDAO categoryDao = new CategoryDAOImpl();
+        List<String> categoriesList = categoryDao.getCategories();
+        //ArrayList<Category> categories;
+        CategoryDAOImpl categoryDAOImpl = new CategoryDAOImpl();
+        //categories = (ArrayList<Category>) categoryDAOImpl.getCategories();
 
-        public PlaceholderFragment() {
+        ArrayList<String> categoriesName = new ArrayList<>();
+        for (int i = 0; i < categoriesList.size(); i++){
+            //categoriesName.add(categoriesList.get(i).getName());
+            categoriesList.add(categoriesList.get(i));
         }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            CategoryDAO categoryDao = new CategoryDAOImpl();
-            List<String> categoriesList = categoryDao.getCategories();
-            //ArrayList<Category> categories;
-            CategoryDAOImpl categoryDAOImpl = new CategoryDAOImpl();
-            //categories = (ArrayList<Category>) categoryDAOImpl.getCategories();
+        categoryAdapter =
+                new ArrayAdapter<String>(
+                        this, // The current context (this activity)
+                        R.layout.list_item_category, // The name of the layout ID.
+                        R.id.list_item_category_textview, // The ID of the textview to populate.
+                        categoriesList);
 
-            ArrayList<String> categoriesName = new ArrayList<>();
-            for (int i = 0; i < categoriesList.size(); i++){
-                //categoriesName.add(categoriesList.get(i).getName());
-                categoriesList.add(categoriesList.get(i));
-            }
-
-            categoryAdapter =
-                    new ArrayAdapter<String>(
-                            getActivity(), // The current context (this activity)
-                            R.layout.list_item_category, // The name of the layout ID.
-                            R.id.list_item_category_textview, // The ID of the textview to populate.
-                            categoriesList);
-
-            View rootView = inflater.inflate(R.layout.categories_fragment_main, container, false);
-
-            // Get a reference to the ListView, and attach this adapter to it.
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_categories);
-            listView.setAdapter(categoryAdapter);
-
-            //Connection c = new Connection();
-
-            //c.execute("http://eatmeet.herokuapp.com/api/categories");
-
-            return rootView;
-        }
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) findViewById(R.id.listview_categories);
+        listView.setAdapter(categoryAdapter);
     }
 }
