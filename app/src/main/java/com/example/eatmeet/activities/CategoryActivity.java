5c0296113b7109d73e10f311dab8f03731fd93ce
Umberto Peserico <umberto.peserico@gmail.com>
2016-06-15 16:45:21 +0200

    Category Backend connection moved to DAO

--- app/src/main/java/com/example/eatmeet/activities/CategoryActivity.java
+++ app/src/main/java/com/example/eatmeet/activities/CategoryActivity.java
@@ -1,54 +1,35 @@
 package com.example.eatmeet.activities;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.eatmeet.R;
 import com.example.eatmeet.dao.CategoryDAO;
 import com.example.eatmeet.dao.CategoryDAOImpl;
-import com.example.eatmeet.entities.Category;
 import com.example.eatmeet.utils.Notificable;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created by sofia on 13/06/2016.
- */
 public class CategoryActivity extends AppCompatActivity implements Notificable {
+
     private ArrayAdapter<String> categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         CategoryDAO categoryDao = new CategoryDAOImpl(this);
         List<String> categoriesList = categoryDao.getCategories();
-        //ArrayList<Category> categories;
-        //categories = (ArrayList<Category>) categoryDAOImpl.getCategories();
-
-        ArrayList<String> categoriesName = new ArrayList<>();
-        for (int i = 0; i < categoriesList.size(); i++){
-            //categoriesName.add(categoriesList.get(i).getName());
-            categoriesList.add(categoriesList.get(i));
-        }
 
         categoryAdapter =
                 new ArrayAdapter<String>(
