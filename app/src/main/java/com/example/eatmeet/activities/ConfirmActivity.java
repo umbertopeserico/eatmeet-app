73de3b1e280f5bac7d2071320217c34102bf8199
zaifoski <sofiachimirri@gmail.com>
2016-06-26 18:56:49 +0200

    add confirmation static content

--- app/src/main/java/com/example/eatmeet/activities/ConfirmActivity.java
+++ app/src/main/java/com/example/eatmeet/activities/ConfirmActivity.java
@@ -1,12 +1,16 @@
 package com.example.eatmeet.activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import com.example.eatmeet.eatmeet.R;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.eatmeet.R;
 
 public class ConfirmActivity extends AppCompatActivity {
 
@@ -17,12 +21,22 @@ public class ConfirmActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Bundle extras = getIntent().getExtras();
+        int eventId = 1;
+        if(extras!=null) {
+            eventId = extras.getInt("id");
+        }
+
+        Button bookButton = (Button) findViewById(R.id.bookButton);
+        bookButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hai prenotato";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
     }
