4115642c9c7429ea8d22f72fb68bab2b608bedb1
zaifoski <sofiachimirri@gmail.com>
2016-07-25 19:57:57 +0200

    fix google maps permission problem

--- app/src/main/java/com/example/eatmeet/mainactivityfragments/GoogleMapFragment.java
+++ app/src/main/java/com/example/eatmeet/mainactivityfragments/GoogleMapFragment.java
@@ -2,11 +2,13 @@ package com.example.eatmeet.mainactivityfragments;
 
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -41,7 +43,7 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class GoogleMapFragment extends Fragment implements GoogleMap.OnInfoWindowClickListener,OnMapReadyCallback, Notificable {
-
+    final int MY_PERMISSIONS_REQUEST_ACCESS_POSITION = 12;
     MapView mapView;
     HashMap<Marker,Integer> markers = new HashMap<>();
     GoogleMap map;
@@ -92,7 +94,8 @@ public class GoogleMapFragment extends Fragment implements GoogleMap.OnInfoWindo
         }
 
         if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            System.out.println("ERROR 2");
+            //ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_POSITION);
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_POSITION);
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -102,6 +105,10 @@ public class GoogleMapFragment extends Fragment implements GoogleMap.OnInfoWindo
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        ifAllowedGetData();
+    }
+
+    private void ifAllowedGetData(){
         System.out.println("MAP READY TO LOAD DATA");
         RestaurantDAO restaurantDAO = new RestaurantDAOImpl(this);
         restaurantList = restaurantDAO.getRestaurants();
@@ -129,9 +136,49 @@ public class GoogleMapFragment extends Fragment implements GoogleMap.OnInfoWindo
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_POSITION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    System.out.println("permission was granted, yay! Do the");
+                    ifAllowedGetData();
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    System.out.println("permission denied, boo! Disable the");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
     public void sendNotify() {
         System.out.println("ENDLOAD DATA. STARTING CALLBACK");
         if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                //if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                //} else {
+                    // No explanation needed, we can request the permission.
+                    //ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_POSITION);
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_POSITION);
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                //}
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
