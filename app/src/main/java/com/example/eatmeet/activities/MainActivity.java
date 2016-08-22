3c20d57ce54144faa2cdfe569c6f6e83fe75ad06
Umberto Peserico <umberto.peserico@gmail.com>
2016-08-22 14:51:46 +0200

    Change implemetation of PageAdapter

--- app/src/main/java/com/example/eatmeet/activities/MainActivity.java
+++ app/src/main/java/com/example/eatmeet/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.eatmeet.activities;
 
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 
@@ -19,6 +20,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -76,9 +78,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private DrawerLayout mDrawerLayout;
 
     public void setCurrentFragment(int position){
-        Refreshable refreshable = (Refreshable) mSectionsPagerAdapter.getItem(position);
-        refreshable.refresh();
         mViewPager.setCurrentItem(position, true);
+        Refreshable refreshable = (Refreshable) mSectionsPagerAdapter.getCurrentFragment();
+        if(refreshable!=null)
+            refreshable.refresh();
     }
 
     @Override
@@ -142,6 +145,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 }
             }
         });
+
+        Intent intent = getIntent();
+        if(intent != null && intent.getExtras() != null) {
+            String applyFilters;
+            applyFilters = (String) intent.getExtras().get("applyFilters");
+            if (applyFilters!= null) {
+                if (applyFilters.equals("1")) {
+                    EatMeetApp.getFiltersManager().setEnabled(true);
+                }
+            }
+
+            String destination;
+            destination = (String) intent.getExtras().get("destination");
+            System.out.println("DESTINATION: "+destination);
+            if (destination!= null) {
+                setCurrentFragment(Integer.parseInt(destination));
+            }
+        }
     }
 
     @Override
@@ -231,25 +252,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        private Fragment mCurrentFragment;
 
-        private List<Fragment> fragments = new ArrayList<>();
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
-            fragments.add(new CategoriesFragment());
-            fragments.add(new EventsFragment());
-            fragments.add(new GoogleMapFragment());
         }
 
         @Override
         public Fragment getItem(int position) {
-            Log.i("FRAGMENT TOSTRING", fragments.get(position).toString());
-            return fragments.get(position);
+            switch (position) {
+                case 0:
+                    return new CategoriesFragment();
+                case 1:
+                    return new EventsFragment();
+                case 2:
+                    return new GoogleMapFragment();
+                default:
+                    return new CategoriesFragment();
+            }
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
             return 3;
         }
 
@@ -264,6 +289,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     return getString(R.string.map_tab_title);
             }
             return null;
+        }
+
+        public Fragment getCurrentFragment() {
+            return mCurrentFragment;
+        }
+
+        @Override
+        public void setPrimaryItem(ViewGroup container, int position, Object object) {
+            if (getCurrentFragment() != object) {
+                mCurrentFragment = ((Fragment) object);
+            }
+            super.setPrimaryItem(container, position, object);
         }
     }
 
@@ -297,23 +334,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onResume() {
         super.onResume();
-        Intent intent = getIntent();
-        if(intent != null && intent.getExtras() != null) {
-            String applyFilters;
-            applyFilters = (String) intent.getExtras().get("applyFilters");
-            if (applyFilters!= null) {
-                if (applyFilters.equals("1")) {
-                    EatMeetApp.getFiltersManager().setEnabled(true);
-                }
-            }
-
-            String destination;
-            destination = (String) intent.getExtras().get("destination");
-            System.out.println("DESTINATION: "+destination);
-            if (destination!= null) {
-                setCurrentFragment(Integer.parseInt(destination));
-            }
-        }
         setMenuLayout();
     }
 }
