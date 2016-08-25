e9278674ae626416e9b8a3a41a264c939f4aa57d
zaifoski <sofiachimirri@gmail.com>
2016-08-25 23:41:27 +0200

    aspect of confirmation activity

--- app/src/main/java/com/example/eatmeet/activities/ConfirmActivity.java
+++ app/src/main/java/com/example/eatmeet/activities/ConfirmActivity.java
@@ -1,16 +1,34 @@
 package com.example.eatmeet.activities;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.eatmeet.EatMeetApp;
 import com.example.eatmeet.R;
+import com.example.eatmeet.backendstatuses.BackendStatusListener;
+import com.example.eatmeet.backendstatuses.BackendStatusManager;
+import com.example.eatmeet.dao.interfaces.EventDAO;
+import com.example.eatmeet.entities.Event;
+import com.example.eatmeet.utils.Visibility;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConfirmActivity extends AppCompatActivity {
 
@@ -20,6 +38,8 @@ public class ConfirmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_confirm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        /*add back button*/
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Bundle extras = getIntent().getExtras();
         int eventId = 1;
@@ -27,17 +47,82 @@ public class ConfirmActivity extends AppCompatActivity {
             eventId = extras.getInt("id");
         }
 
-        Button bookButton = (Button) findViewById(R.id.bookButton);
+        Button homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConfirmActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final Button bookButton = (Button) findViewById(R.id.bookButton);
         bookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final Spinner spinnerPeople = (Spinner) findViewById(R.id.spinnerPeople);
+                Integer bookedPeople = (Integer) spinnerPeople.getSelectedItem();
+                //TODO: fai la prenotazione
                 Context context = getApplicationContext();
-                CharSequence text = "Hai prenotato";
+                CharSequence text = "Hai prenotato per " + bookedPeople + " persone";
                 int duration = Toast.LENGTH_SHORT;
 
                 Toast toast = Toast.makeText(context, text, duration);
+                TextView bookedFeedback = (TextView) findViewById(R.id.bookedFeedback);
+                bookedFeedback.setText(text);
+                Visibility.makeVisible(bookedFeedback);
+                Visibility.makeVisible(findViewById(R.id.homeButton));
                 toast.show();
+
+                bookButton.setBackgroundColor(Color.parseColor("#cccccc"));
+                bookButton.setEnabled(false);
+                findViewById(R.id.spinnerPeople).setEnabled(false);
             }
         });
+
+        final EventDAO eventDAO = EatMeetApp.getDaoFactory().getEventDAO();
+        final BackendStatusManager backendStatusManager = new BackendStatusManager();
+        backendStatusManager.setBackendStatusListener(new BackendStatusListener() {
+            @Override
+            public void onSuccess(Object response, Integer code) {
+                final Spinner spinnerPeople = (Spinner) findViewById(R.id.spinnerPeople);// Array of choices
+                List<Integer> numberPeopleList = new ArrayList<>();
+                for(int i = 1; i <= ((Event) response).getMaxPeople() - ((Event) response).getParticipantsCount(); i++){
+                    numberPeopleList.add(i);
+                }
+                ArrayAdapter<Integer> spinnerArrayAdapter = new ArrayAdapter(getApplicationContext(), R.layout.spinner_item, numberPeopleList);
+                spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerPeople.setAdapter(spinnerArrayAdapter);
+                spinnerPeople.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        changePeopleNumber(position+1);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                    }
+                });
+            }
+            @Override
+            public void onFailure(Object response, Integer code) {
+            }
+        });
+        eventDAO.getEvent(eventId,backendStatusManager);
+    }
+
+    private void changePeopleNumber(int peopleNumber){
+        DecimalFormat df = new DecimalFormat("#.##");
+        String estimate = df.format(Math.random()*10) + "€ a persona se prenoti per " + peopleNumber + " persone.";//TODO
+        TextView dynamicPrice = (TextView) findViewById(R.id.dynamicPrice);
+        dynamicPrice.setText(estimate);
+        Toast.makeText(ConfirmActivity.this, estimate, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
     }
 }
